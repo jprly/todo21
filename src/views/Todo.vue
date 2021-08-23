4ec1b5df0cbfabafa48ee8771433d92e77846444
Miranda Riley <mirandariley@Mirandas-Air-2.attlocal.net>
2021-08-22 21:11:05 -0500

    all list functionality added

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -2,6 +2,8 @@
   <h1>Todo</h1>
   <p>Folder id: {{route.params.folderId}}</p>
   <p>Todo id: {{route.params.todoId}}</p>
+<input type="text" v-model="newName">
+  <Button @click="nameUpdate">update</Button>
 </template>
 
 <script setup>
@@ -16,7 +18,18 @@ const store = inject("store");
 const state = inject("store").state.value;
 
 const methods = inject("store").methods;
-
+const newName = ref('')
+const nameUpdate = () => {
+  console.log(state.folders[route.params.folderId])
+  // show name update
+  state.folders[route.params.folderId].todos.map((item) => {
+    console.log(item.uid, '===', route.params.todoId)
+    if(item.uid == route.params.todoId){
+      item.title = newName.value;
+      console.log('updated with', newName.value)
+    }
+  })
+}
 
 console.log(route.params.folderId)
 </script>
